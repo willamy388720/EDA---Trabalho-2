d2a30ca1f65a2b56cb32a2674da74016f0ed51e0
pedrinho <pedroccufc@gmail.com>
2017-03-30 11:19:41 -0300

    Implementação da classe heap ternario

--- src/br/ufc/quixada/eda/listaprioridades/HeapTernario.java
+++ src/br/ufc/quixada/eda/listaprioridades/HeapTernario.java
@@ -14,7 +14,7 @@ public class HeapTernario {
 	}
 	
 	private void subir(int i){
-		int p = i/2;
+		int p = i/3;
 		if(p >= 1){
 			if(vetor[p] < vetor[i]){
 				int aux = vetor[i];
@@ -26,18 +26,20 @@ public class HeapTernario {
 	}
 	
 	private void descer(int i){
-		int f = i * 2;
-		if(f <= n){
-			if((f + 1) <= n){
-				if(vetor[f] < vetor[f + 1]){
-					f++;
+		int f = i * 3;
+		if((f - 1) <= n){
+			if(f <= n){
+				if((f + 1) <= n){
+					if((vetor[i - 1] < vetor[i]) && (vetor[f] < vetor[f + 1])){
+						f++;
+					}
 				}
-			}
-			if(vetor[f] > vetor[i]){
-				int aux = vetor[f];
-				vetor[f] = vetor[i];
-				vetor[i] = aux;
-				descer(f);
+				if(vetor[f] > vetor[i]){
+					int aux = vetor[f];
+					vetor[f] = vetor[i];
+					vetor[i] = aux;
+					descer(f);
+				}
 			}
 		}
 	}
@@ -47,8 +49,10 @@ public class HeapTernario {
 			vetor[i] = entrada.get(i - 1);
 			n++;
 		}
-		for(int i = (n/2); i > 1; i--){
+		for(int i = (n/3); i > 1; i--){
+			descer(i - 1);
 			descer(i);
+			descer(i + 1);
 		}
 	}
 	
